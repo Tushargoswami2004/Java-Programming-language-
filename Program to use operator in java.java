72fe9187a8ae495d2e0b590bd71ef1ff6b920351
Tushar Goswami <tushargoswamid@gmail.com>
2023-09-22 18:47:06 +0530

    Add files via upload
    how to use Arithmetic operator in java

---
+++ Program to use operator in java.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class JavaBasics{
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Addition =  " + (a + b));
+        System.out.println("Subtract =  " + (a - b));
+        System.out.println("multiply =  " + (a * b));
+        System.out.println("Divide = " + (a / b));
+        System.out.println("Remeinder = " + (a % b));
+
+    }
+
+}
